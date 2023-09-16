66ea94362116008f2ce1058471cfbf6e18369ec6
brennobranco <brancobrenno010@gmail.com>
2023-09-16 14:58:41 -0300

    update

--- js/script.js
+++ js/script.js
@@ -4,10 +4,21 @@ const counter = document.querySelector('.counter')
 const menu = document.querySelector('.menu')
 const button = document.querySelector('.play-again')
 const menuScore = document.querySelector('.menu-score')
-const sky = document.querySelector('.tela')
+const screenGame = document.querySelector('.tela')
 const cloud = document.querySelector('.cloud')
 let armazenador = JSON.parse(localStorage.getItem('score'));
 let time = 1;
+let width = window.screen.width;
+let height = window.screen.height;
+
+
+if(height > width){
+    screenGame.style.transform = 'rotate(90deg)'
+    screenGame.style.height = `${width}px`
+    screenGame.style.width = '100%'
+    pipe.style.animation = 'pipe-animation 0.8s linear infinite'
+}
+
 
 const jump = () =>{
     mario.classList.add('jump')
@@ -46,35 +57,11 @@ const setMenu = () => {
 
 
   const loop = setInterval(() =>{
-
     const pipePosition = pipe.offsetLeft;
     const marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px', ''));
-    // console.log(marioPosition)
-    // console.log(pipePosition)
     if(setScore){
         setScore()
     }
-    // if(time > 50 && time < 100 || time > 150 && time < 200){
-    //     sky.style.background = 'rgb(33, 33, 33)'
-    //     cloud.style.opacity = '0.5'
-    // }
-    // if(time > 100 && time < 150 || time >200){               //tentativa de mudar o back, mas transition n funfa
-    //     sky.style.background = ''
-    //     cloud.style.opacity = '' 
-    //     sky.style.transition = ''
-    // }
-    // if(time > 50 && time <= 100 && pipePosition > 90){
-    //     pipe.style.animation = 'pipe-animation 1.5s linear infinite'
-    // }
-    // if(time > 100 && time <= 200 && pipePosition > 90){
-    //     pipe.style.animation = 'pipe-animation 1s linear infinite'
-    // }
-    // if(time > 200 && time <= 300 && pipePosition > 90){              //tentativa de acelerar o pipe, mas buga 
-    //     pipe.style.animation = 'pipe-animation 0.7s linear infinite'
-    // }
-    // if(time > 300 && pipePosition > 90){
-    //     pipe.style.animation = 'pipe-animation 0.5s linear infinite'
-    // }
     if(pipePosition <= 134 && pipePosition > 0 && marioPosition <= 96){
 
         pipe.style.animation = 'none'
@@ -102,6 +89,7 @@ const setMenu = () => {
     }
 
   }, 10)
+document.addEventListener('touchstart', jump)
 document.addEventListener('keydown', jump)
 button.addEventListener('click', removeMenu)
 
